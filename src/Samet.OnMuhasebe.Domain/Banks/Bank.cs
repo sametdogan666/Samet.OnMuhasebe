be12c98cd6ba75f55c4a2ab59aff5848b5fa2827
Samet DOGAN <sametdogan23@protonmail.ch>
2024-03-30 19:47:47 +0300

    bank entity created

---
+++ src/Samet.OnMuhasebe.Domain/Banks/Bank.cs
@@ -0,0 +1,12 @@
+﻿namespace Samet.OnMuhasebe.Banks;
+
+public class Bank : FullAuditedAggregateRoot<Guid>
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public Guid? SpecialCode1Id { get; set; }
+    public Guid? SpecialCode2Id { get; set; }
+    public string Description { get; set; }
+    public bool Status { get; set; }
+
+}
